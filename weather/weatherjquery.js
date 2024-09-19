af6a6eff6632d2b82638caaef88accc9aa617365
seok345 <tkddbstjrdbs123@g.shingu.ac.kr>
2024-09-19 11:33:42 +0900

    gg

--- weather/weatherjquery.js
+++ weather/weatherjquery.js
@@ -2,7 +2,7 @@ let WeatherObject = {
 	getWeather: function() {
 		$.ajax({
 			type: "GET",
-			url: 'https://api.openweathermap.org/data/2.5/weather?q=Seoul&units=metric&appid=7d96bc5108f52b80e2d9075a369b9f35',
+			url: 'https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=7d96bc5108f52b80e2d9075a369b9f35',
 		}).done(function(response) {
             let wdata = response
             let exdata = response.weather[0];
