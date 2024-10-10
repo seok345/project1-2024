95e051eb047a886c989209db327bd6c92d55097e
seok345 <tkddbstjrdbs123@g.shingu.ac.kr>
2024-10-10 12:26:20 +0900

    제발

--- googlrVision/vision.js
+++ googlrVision/vision.js
@@ -1,14 +1,12 @@
-//https://github.com/gang-0730/project1-2024
-CV_URL = 'https://vision.googleapis.com/v1/images:annotate?key=' + GOOGLE_API_KEY;
+//https://github.com/yoyoshingu/project1-2024
 
 GOOGLEAPI_KEY = "AIzaSyBZCWwGS6gDDuBfqhiilC33DuNjWgp8taE"
+CV_URL = 'https://vision.googleapis.com/v1/images:annotate?key=' + GOOGLEAPI_KEY;
 function processFile(event){
-    content = event.target.result
-    imagestring = content.replace('data:image/jpeg;base64,','')
+    content = event.target.result 
+    imagestring = content.replace('data:image/jpeg;base64,', '')
     document.getElementById("gimage").src = content
-
 }
-
 
 function uploadFiles(files){
     file = files[0]
@@ -21,32 +19,29 @@ function analyze(){
     data ={
         requests: [{
             image:{
-                content: imagestirng
+                content: imagestring
             },
             features:[{
-                type:"FACE__DETECTION",
-                maxResults:100
+                type:"FACE_DETECTION",
+                maxResults: 100
             }]
         }]
-            }
-    
+    }
 
     $.ajax({
         type:"POST",
-        url: "POST https://vision.googleapis.com/v1/images:annotate" + "?key="+ GOOGLEAPI_KEY,
+        url:'https://vision.googleapis.com/v1/images:annotate?key=' + GOOGLEAPI_KEY,
         headers:{
-            "Accept" : "application/json",
-            "Content-Type" : "application/json"
+            "Accept": "application/json",
+            "Content-Type": "application/json"
         },
         data: JSON.stringify(data),
         contentType: "application/json; charset=utf-8"
     }).done( function(response){
         console.log(response)
-        
+
     }).fail(function(error){
         console.log(error)
-       
-    }
 
-    )
+    })
 }
